c844ea0f39f42251311427ad1442bad157b985cf
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-01-04 02:41:29 +0900

    fix: formatDate

--- src/client/foundation/pages/Top/Top.jsx
+++ src/client/foundation/pages/Top/Top.jsx
@@ -20,7 +20,9 @@ const formatDate = (date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  return `${year}-${month}-${day}`;
+  return `${year}-${month.toString().padStart(2, "0")}-${day
+    .toString()
+    .padStart(2, "0")}`;
 };
 
 /**
